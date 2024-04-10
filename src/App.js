2e5d154d5b50857dc3b910b5a8d2482da0eea432
Sdg877 <sdrakegill@gmail.com>
2024-04-10 11:40:38 +0100

    project setup, logo and contact info added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import emLogo from './em_logo.png'
+import instagramLogo from './instagram.png'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <img className="logo" src={emLogo} alt="E. M. Makes logo" />
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <div className='contact'>
+      <h2>Dunmanway, Co.Cork</h2>
+      <h3>Tel: +353 (0) 83 012 5037</h3>
+      <h3>emily.montgomery.makes@gmail.com</h3>
+      </div>
+      <div>
+      <a href="https://www.instagram.com/e.m.makes" target="_blank" rel="noopener noreferrer">
+          <img className='contact-icon'src={instagramLogo} alt="Instagram logo"/>
+      </a>
+      </div>
     </div>
   );
 }
