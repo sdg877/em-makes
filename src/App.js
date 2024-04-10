9e564538c5c3bb5b8394e6e243d918e09965d5d7
Sdg877 <sdrakegill@gmail.com>
2024-04-10 17:46:18 +0100

    styling updates, photos added

--- src/App.js
+++ src/App.js
@@ -1,27 +1,42 @@
 import './App.css';
-import emLogo from './em_logo.png'
-import instagramLogo from './instagram.png'
+import emLogo from './images/background.jpeg';
+import chair1 from './images/chair1.jpeg';
+import chair2 from './images/chair2.jpeg';
+import chair3 from './images/chair3.jpeg';
+import chair4 from './images/chair4.jpeg';
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
 
 function App() {
+  const chairImages = [chair1, chair2, chair3, chair4];
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-        <img className="logo" src={emLogo} alt="E. M. Makes logo" />
-        </p>
-      </header>
       <div className='contact'>
-      <h2>Dunmanway, Co.Cork</h2>
-      <h3>Tel: +353 (0) 83 012 5037</h3>
-      <h3>emily.montgomery.makes@gmail.com</h3>
+        <img className="logo" src={emLogo} alt="E. M. Makes logo" />
+        <h1>Professional Upholstery Service 
+          <br></br>
+          by Emily Montgomery.
+        </h1>
+        <h2>Based in Dunmanway, covering West Cork.</h2>
+        <h3>Tel: +353 (0) 83 012 5037</h3>
+        <h3><a href="mailto:emily.montgomery.makes@gmail.com" style={{ color: 'white' }}>emily.montgomery.makes@gmail.com</a></h3>
+        <h3>
+          <a href="https://www.instagram.com/e.m.makes" target="_blank" rel="noopener noreferrer" style={{ color: 'white'}}>
+            @e.m.makes
+          </a>
+        </h3>
       </div>
-      <div>
-      <a href="https://www.instagram.com/e.m.makes" target="_blank" rel="noopener noreferrer">
-          <img className='contact-icon'src={instagramLogo} alt="Instagram logo"/>
-      </a>
-      </div>
+      <Carousel showThumbs={false} showStatus={false} dynamicHeight={true} autoPlay={true} interval={10000} infiniteLoop={true}>
+        {chairImages.map((image, index) => (
+          <div key={index}>
+            <img className="carousel-image" src={image} alt={`Chair ${index + 1}`} />
+          </div>
+        ))}
+      </Carousel>
     </div>
   );
 }
 
 export default App;
+
